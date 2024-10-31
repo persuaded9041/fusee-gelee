afa41d9aab1dba2a5a0f30afde989ab25d369bca
conradson <464398+conradson@users.noreply.github.com>
2024-10-31 20:03:25 +0100

    bump versions to 6.2.2

--- fuseegelee.sh
+++ fuseegelee.sh
@@ -1,1 +1,1 @@
-sudo python3 ./fusee-launcher.py hekate_ctcaer_6.1.1.bin
+sudo python3 ./fusee-launcher.py hekate_ctcaer_6.2.2.bin
--- nsusbloader.sh
+++ nsusbloader.sh
@@ -1,1 +1,1 @@
-sudo java -jar ns-usbloader-7.1.jar
+sudo java -jar ns-usbloader-7.2.jar
